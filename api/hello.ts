e5b1d68d77b271b1f1a29f97e276d0f8d5f63502
Ryohei Watanabe <watanaber17@gmail.com>
2023-06-03 16:36:54 +0900

    commit

--- api/hello.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-import credentials from "../credentials.json"
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  console.log("HELLO")
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
-}
